1fadf0f682b9b08879af64be0b78e6e4c705fd15
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-19 00:17:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var name = "Joe"
+var name = "Susan"
 if (name === "Susan") {
   console.log("The name is correct")
-}
-else { console.log("Expected the name to be Susan")
+} else { 
+  console.log("Expected the name to be Susan")
 } 
 var height = "74"
 
